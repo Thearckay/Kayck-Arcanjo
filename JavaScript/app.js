e180936ddb570ed457b86658fd610b75912bbe3f
Thearckay <thearckay@gmail.com>
2025-05-01 15:04:50 -0300

    Rodapé + js add
    Adicionei um rodapé simples + alguns cálculos, de ano e idade, para mudarem automaticamente conforme o tempo passa

--- JavaScript/app.js
+++ JavaScript/app.js
@@ -1,1 +1,15 @@
-alert('Olá, seja bem-vindo(a), ao meu site/Portifólio Online. Ainda está em Desenvolvimento!');
+// alert('Olá, seja bem-vindo(a), ao meu site/Portifólio Online. Ainda está em Desenvolvimento!');
+
+const anohtml = document.querySelector('.ano')
+let data = new Date()
+
+// Calcular ano
+let anoAtual = data.getFullYear();
+anohtml.textContent = anoAtual
+
+// Calcular minha idade
+
+const idadehtml = document.querySelector('.idade')
+const birthDate = 2005;
+idadehtml.textContent = anoAtual-birthDate
+
