db08641781d4c38d7c58b0cab1958fa43b0f04de
Thearckay <thearckay@gmail.com>
2025-05-11 14:52:09 -0300

    Atualização 1.8
    Adicionado algumas interação nos botões que terão funcionalidades especificas
    
    + Adição de botão de informação do BootStrap Icons - Na sessão de Skills.

--- JavaScript/app.js
+++ JavaScript/app.js
@@ -11,3 +11,39 @@ const idadehtml = document.querySelector('.idade')
 const birthDate = 2005;
 idadehtml.textContent = anoAtual-birthDate
 
+// AVISOS DO HEADER
+
+let headerCurriculumButton = document.getElementById('headerCurriculumButton'); //Botão curriculo do menu
+headerCurriculumButton.addEventListener('click', emDesenvolvimento)
+
+let headerProjectsButton = document.getElementById('headerProjectsButton'); // Botão projetos
+headerProjectsButton.addEventListener('click', emDesenvolvimento)
+
+let headerAboutMeButton = document.getElementById('headerAboutMeButton');
+headerAboutMeButton.addEventListener('click', emDesenvolvimento)
+
+function emDesenvolvimento() {
+    alert('Olá, essa função ainda está em desenvolvimento! Logo estará disponivel, aguarde.')
+}
+
+
+
+// ========================================================
+
+//Interação no icone de informação da sesão skills
+
+let infoSkillsButton = document.getElementById('infoIcon'); //Botão 
+infoSkillsButton.addEventListener('click', showPopUp) // Interação com o botão
+
+function showPopUp() {
+    let popUpDisplay = document.getElementById('popup');
+    popUpDisplay.style.display = 'block';
+}
+
+let closePopUpButton = document.getElementById('closePopUp');
+closePopUpButton.addEventListener('click', closePopDisplay)
+
+function closePopDisplay() {
+    let popUpDisplay = document.getElementById('popup');
+    popUpDisplay.style.display = 'none';
+}
--- README.md
+++ README.md
@@ -1,4 +1,7 @@
 # Kayck-Arcanjo
 Olá, seja bem-vindo ao meu repositório! Aqui está a minha LandingPage/Portifólio.
 
-Fiz com o intuito de treinar minhas habilidades com HTML5, CSS3 e JavaScript
+Fiz com o intuito de treinar minhas habilidades com HTML5, CSS3 e JavaScript. Além de ser uma forma de pessoas, recrutadores poderem conhecer na internet
+
+- As informações que envolvem tempo (Como: Data e idade), são processados pelo javascript no arquivo 'app.js', (utilizei a tag 'span' do HTML para integra-los ao js)
+
