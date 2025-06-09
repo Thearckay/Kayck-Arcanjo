083c35f8b44de9fd016d21530eb773c654d1b918
Kayck Arcanjo <thearckay@gmail.com>
2025-06-09 10:23:08 -0300

    Versão 2.4.2
    Implementação da sessão de 'Curriculo' em formato de slide. (Ainda testando/aprendendo).
    
    Muito provavelmente existem formas mais adequadas de se fazer isso, mas já estou feliz visto que estou iniciando na área :)

--- JavaScript/app.js
+++ JavaScript/app.js
@@ -23,14 +23,65 @@ function emDesenvolvimento() {
     alert('Olá, essa função ainda está em desenvolvimento! Logo estará disponível, aguarde.')
 }
 
-// Botão menu para Mobile
+//Slide
 
+let slideCurrent = 1
 
+//botões
+const leftButton = document.getElementById('leftButton')
+const rightButton = document.getElementById('rightButton')
+const slide1 = document.getElementById('slide1')
+const slide2 = document.getElementById('slide2')
+const slide3 = document.getElementById('slide3')
+const slidePic = document.getElementById('slidePic')
 
+leftButton.addEventListener('click', backSlide )
+rightButton.addEventListener('click', nextSlide )
 
+function backSlide() {
+    console.log('Backslide ativado')
+    slideCurrent = slideCurrent -1
+    changeSlide()
+}
 
+function nextSlide() {
+    slideCurrent = slideCurrent +1
+    changeSlide()
+}
 
+function changeSlide(){
 
+ 
+    if ( slideCurrent>3) {
+        slideCurrent = 1
+    } else if( slideCurrent < 1){
+        slideCurrent = 3
+    }
+
+    switch (slideCurrent) {
+        case 1:
+            console.log('Clicado caso 1')
+            slide1.classList.remove('slideOff')
+            slide2.classList.add('slideOff')
+            slide3.classList.add('slideOff')
+            break;
+        case 2:
+            console.log('Clicado caso 2')
+            slide2.classList.remove('slideOff')
+            slide1.classList.add('slideOff')
+            slide3.classList.add('slideOff')
+            break
+
+        case 3:
+            console.log('Clicado caso 3')
+            slide3.classList.remove('slideOff')
+            slide1.classList.add('slideOff')
+            slide2.classList.add('slideOff')
+            break
+        default:
+            break;
+    }
+}
 
 
 
